7d98e4c99b70ee1c1a38eafb05783105bb082a94
daonb <bennydaon@gmail.com>
2011-10-19 22:48:13 +0200

    started using jslint.vim. js much better now

--- js/publisher.js
+++ js/publisher.js
@@ -1,27 +1,31 @@
 function showContent(hash) {
-    var docs = {}
-    if(hash == "") {
+    var docs = {};
+    if (hash === "") {
         // take the clicks
         $('.hash-click').click( function (e) {
             var url = $(this).attr('href');
             url = url.replace(/^.#/, '');
             $.history.load(url);
             return false;
-        })
+        });
         // get the data
         $.get("publisher/var/docs.json" , function (data) {
             docs = data.sort(function(x,y) { 
-                if (x['updated']<y['updated']) return 1;
-                if (x['updated']>y['updated']) return -1;
+                if (x.updated<y.updated) { return 1; }
+                if (x.updated>y.updated) { return -1;}
                 return 0;
             });
-            var t = $('#doc_table_template').html()
+            var t = $('#doc_table_template').html();
             $('#docs').html(Mustache.to_html(t, {docs: docs}));
-        }, 'json')
+        }, 'json');
     } else {
         // restore the state from hash
-        // for (int i=0; i<len(docs); i++) {
-            
+        var i = {};
+        for (i in docs) {
+            if (i.slug == hash) {
+                return false;
+            }
+        }
     }
 }
 
