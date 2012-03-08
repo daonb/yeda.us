0b0d64d2cfbdc56915e177f05f879ee706d611fa
daonb <bennydaon@gmail.com>
2012-03-08 22:50:46 +0200

    crawling...

--- js/yedaTable.js
+++ js/yedaTable.js
@@ -1,40 +1,90 @@
 // var ALL_DOCS_URL = "http://yeda.iriscouch.com/yeda_home/_all_docs?callback=?";
 var allDocsUrl = "static/all_docs.json";
-var destDocTemplate = $('#dest-doc-template').html()
-var destLinkTemplate = $('#dest-link-template').html()
+var destDocTemplate = $('#dest-doc-template').html();
+var destDocDetailsTemplate = $('#dest-doc-details-template').html();
+var destLinkTemplate = $('#dest-link-template').html();
+var fadingMsec = 100;
 var docPrefix = ['doc', 'spr'];
+
+function generateHash(slug) {
+    if (slug)
+        return ""+slug
+    else
+        return "";
+}
+// Generate a fully qualified url for a given slug
+function generate_url(slug) {
+    return "http://#"+window.location.host+"#"+xgenerate_hash(slug);
+}
+
+// Change page's hash - this is the way we keep (and update) our current state
+function update_history(slug) {
+    setTimeout(function (slug) {
+        window.location.hash = generate_hash(slug);
+        }, 0, slug);
+}
+
+function clr() {
+    $('.items :visible').fadeToggle(fadingMsec);
+}
 
 $.Isotope.prototype._positionAbs = function( x, y ) {
   return { right: x, top: y };
 };
 
 $(document).ready(function () {
+        function onhashchange() {
+            var slug = window.location.hash.slice(1);
+            if (slug.length > 0) {
+                clr();
+                $('.item-details[doc_id|="'+slug+'"]"').fadeToggle(fadingMsec);
+            }
+            else {
+                clr();
+                $('.item').fadeToggle(fadingMsec);
+            }
+
+        }
+        window.onhashchange = onhashchange;
+
         $.getJSON (allDocsUrl,
-        {include_docs: true},
-        function (data) {
+                   {include_docs: true},
+                   function (data) {
             // got the data, time to create the divs...
-            for (var i=0; i<data.rows.length; i++) {
+            for (var i=0; i < data.rows.length; i++) {
                 var doc = data.rows[i].doc;
                 var item = $(document.createElement('div')).
-                    addClass("dest-item").
-                    attr({"doc_id": doc._id, 
-                          "doc_updated": (doc.hasOwnProperty('updated'))?doc.updated:'AAAA'
-                         }). 
-                    width((doc.width)?doc.width:250).
-                    height((doc.height)?doc.height:"auto");
-                // use the a template based on the doc type 
+// new item is brought to the table
+addClass("item").
+attr({"doc_id": doc._id,
+      "doc_updated": (doc.hasOwnProperty('updated'))?doc.updated:'unknown'
+     }).
+width((doc.width)?doc.width:250).
+height((doc.height)?doc.height:"auto");
+                //
+                // use the a template based on doc and link types
                 // TODO: add a doc_type field to the DB
-                if (docPrefix.indexOf(doc._id.substr(0,3)) != -1)
+                if (docPrefix.indexOf(doc._id.substr(0,3)) != -1) {
+                    // we've got a doc. let's render the thumb and 
                     item.html(Mustache.render(destDocTemplate, doc));
-                else
+                    // create an item-details div
+                    $(document.createElement('div')).
+addClass("item-details").
+attr({"doc_id": doc._id }).
+html(Mustache.render(destDocDetailsTemplate, doc));
+                }
+                else {
+                    // we've got a shallow div just give it a thumb
                     item.html(Mustache.render(destLinkTemplate, doc));
-                $('#destinations').append(item);
-            }
+                }
+                $('#items').append(item);
+
+            };
             // draw the logo and
             $('#logo canvas').hLogo();
             // update the grid
-            $('#destinations').isotope({
-                itemSelector: '.dest-item',
+            $('#items').isotope({
+                itemSelector: '.item',
                 transformsEnabled: false,
                 masonry: {columnWidth: 100},
                 getSortData : {
